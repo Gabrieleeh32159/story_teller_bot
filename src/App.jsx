2c2e1ec252cda5b1f214d7e3fdfd13225e324bff
Gabrieleeh32159 <gabriel.espinoza.h@utec.edu.pe>
2023-04-20 13:21:11 -0500

    Now can sign in with google! Must realize how to manage ID's

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import LoginForm from "./components/LoginForm"
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
   return (
     <React.Fragment>
       <div className="p-6 bg-[url('/public/background.jpg')] bg-no-repeat bg-cover h-screen flex justify-center items-center" >
-        <LoginForm></LoginForm>
+        {user ? 
+        <div className="flex flex-col gap-5 items-center">
+          <h1 className="text-white text-6xl"> Welcome!</h1>
+          <img src={user.imageUrl} alt={`Profile image of the user ${user.email}`} />
+          <h2 className="text-white text-2xl">{user.givenName + " " + user.familyName}</h2>
+        </div> : <LoginForm setUser={setUser}></LoginForm>}
       </div>
     </React.Fragment>
   )
